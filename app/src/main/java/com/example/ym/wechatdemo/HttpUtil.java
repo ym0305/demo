1bc03aad0b05ad7bd946442f556a0713b10be1b1
yamin <ym534766545@gmail.com>
2017-03-05 13:45:17 +0800

    添加okhttp类

---
+++ app/src/main/java/com/example/ym/wechatdemo/HttpUtil.java
@@ -0,0 +1,28 @@
+package com.example.ym.wechatdemo;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by YM on 2017/3/5.
+ */
+
+public class HttpUtil {
+
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(address)
+                .build();
+        client.newCall(request).enqueue(callback);
+
+
+    }
+}
